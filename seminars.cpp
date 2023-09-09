a2f7e428e29158e481e52a0d3521d40c3a16bbfc
Vadim <dabizha.vadik@yandex.ru>
2023-09-09 11:03:27 +0300

    yeeeeah

--- seminars.cpp
+++ seminars.cpp
@@ -1,23 +1,81 @@
 
 //1
-#include <iostream>
+// #include <iostream>
 
+// using namespace std;
+
+// int main(){
+//     int n = 0;
+//     int F1=1;
+//     int F2=1;
+//     int k=0;
+
+//     cout<< " Введите n ";
+//     cin>> n;
+    
+//     for (int i=0;i<n-2;i++){
+//         k=F2;
+//         F2=F1+F2;
+//         F1=k;
+//     }
+//     cout<< F2;
+// }
+
+//2
+
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// using namespace std;
+
+// int main(){
+//     string n="";
+//     cout<<" введите число "<< endl;
+//     cin>>n;
+//     for (int i =n.length();i>-1;i--){
+//         cout<< n[i];
+//     }
+// }
+
+
+//3
+
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// #include <cmath>
+// using namespace std;
+
+// int main(){
+//     int n=0;
+//     int m=0;
+//     cout<<" введите число "<< endl;
+//     cin>>n;
+//     for (int i =2;i<= pow(n,0.5);i++){
+//         if( n%i==0){
+//             cout<< " Число не является простым ";
+//             m=0;
+//             break;
+//         }
+//         else{
+//             m=1;
+//         }
+//     }
+//     if ((m=1) or (n =1) or (n=2)){
+//         cout<< " Число простое ";
+//     }
+// }
+
+
+//4
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
 using namespace std;
 
 int main(){
-    int n = 0;
-    int F1=1;
-    int F2=1;
-    int k=0;
-
-    cout<< " Введите n ";
-    cin>> n;
     
-    for (int i=0;i<n-2;i++){
-        k=F2;
-        F2=F1+F2;
-        F1=k;
-    }
-    cout<< F2;
 }
-
