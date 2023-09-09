8230994a483c14682d9595d8855c011bceada568
Vadim <dabizha.vadik@yandex.ru>
2023-09-09 17:21:24 +0300

    ну

--- seminars.cpp
+++ seminars.cpp
@@ -67,8 +67,54 @@
 // }
 
 
-//4
+//4.1
 
+
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// #include <cmath>
+// using namespace std;
+
+// int main(){
+//     float sum=1;
+//     float fact=1;
+//     for (int i =1;i<10;i++){
+//         fact=1;
+//         for (int j =1;j<=2*i;j++){
+//             fact = fact*j;
+            
+//         }
+
+
+//         sum = sum + 1/(fact);
+//         cout<< sum- ((exp(1)+ pow(exp(1),-1))/2)<<endl;
+        
+//     }
+
+
+// }
+
+//4.2
+
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// #include <cmath>
+// using namespace std;
+
+// int main(){
+//     float sum=1;
+//     for (int k=1;k<1000;k++){
+//         sum=sum*4*k*k/(4*k*k-1);
+//         cout<< sum - (M_PI)/2<<endl;
+//     }
+    
+
+// }
+
+
+//4.3
 
 #include <iostream>
 #include <vector>
@@ -76,6 +122,30 @@
 #include <cmath>
 using namespace std;
 
+int factorial(int fact){
+    int frog =1;
+
+    for( int i =1;i<=fact;i++){
+        frog=frog*i;
+
+    }
+    return frog;
+}
 int main(){
+    float e=1;
+    float fact=1;
+    float n=0;
+    for (int k=1;k<100;k++){
+        fact=factorial(k);
+        
+
+        n=1/float(k);
+     
+        e=k/pow(fact,n);
+        
+        cout<< e - exp(1)<<endl;
+        
+    }
     
+
 }
